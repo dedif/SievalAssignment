efcbee7e0aef8ca2a1556bc128f39a748d42429d
Dennis Holierhoek <dennis.hh@hotmail.com>
2023-05-23 23:47:50 +0200

    Added data access layer to dependency injection

--- SievalAssignment/Program.cs
+++ SievalAssignment/Program.cs
@@ -1,8 +1,13 @@
+using SievalAssignment.DAL;
+using SievalAssignment.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AssignmentContext>();
+builder.Services.AddScoped<IArticlesDataAccessLayer, ArticlesDataAccessLayer>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
